83732ad7bd7cb51a4863ed27fb102b2b78e9c7a4
Kiagus Arief Adriansyah <arief@alodokter.com>
2018-10-29 14:40:37 +0700

    moving paper-progress

--- app/javascript/packs/components/question-list.js
+++ app/javascript/packs/components/question-list.js
@@ -75,8 +75,8 @@ class QuestionList extends PolymerElement {
                 on-error='_onDeleteError'>
             </iron-ajax>
 
+            <paper-progress id="progress" hidden indeterminate></paper-progress>
             <div class="flex grid-container" width="100%">
-                <paper-progress id="progress" hidden indeterminate></paper-progress>
                 <vaadin-grid theme="row-stripes" aria-label="Users" items="[[data]]">
                     
                     <vaadin-grid-column width="40%" flex-grow="0">
